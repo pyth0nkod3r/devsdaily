2eb545b0ab3d1b9c8e02498c9c1478fb3df9adfe
pyth0nkod3r <o.amagwula@stu.unizik.edu.ng>
2022-06-30 22:12:00 +0100

    debugging

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -93,11 +93,11 @@ WSGI_APPLICATION = 'newsproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   # 'default': {
-   #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
    # }
-   "default" : env.dj_db_url("DATABASE_URL")
+   #"default" : env.dj_db_url("DATABASE_URL")
 }
 
 
