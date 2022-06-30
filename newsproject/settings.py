6e3adc39f972448c11921031be58a9dc73448ebe
pyth0nkod3r <o.amagwula@stu.unizik.edu.ng>
2022-06-30 22:13:10 +0100

    debugging

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-   # }
+    }
    #"default" : env.dj_db_url("DATABASE_URL")
 }
 
